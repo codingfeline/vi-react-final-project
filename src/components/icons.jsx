fe734381db9ab54b666da82c40a9c5dc6e57c86c
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-01 18:21:27 +0100

    Fontawesome

---
+++ src/components/icons.jsx
@@ -0,0 +1,14 @@
+export { FontAwesomeIcon as Awe } from '@fortawesome/react-fontawesome'
+
+export { faEnvelope as envelope } from '@fortawesome/free-solid-svg-icons'
+export { faHouse } from '@fortawesome/free-solid-svg-icons'
+export { faCircle } from '@fortawesome/free-solid-svg-icons'
+
+export { faCircle as faCircle2 } from '@fortawesome/free-regular-svg-icons'
+export { faLemon } from '@fortawesome/free-regular-svg-icons'
+export { faUtensils as utensils } from '@fortawesome/free-solid-svg-icons'
+export { faCaretDown as down } from '@fortawesome/free-solid-svg-icons'
+export { faCaretUp as up } from '@fortawesome/free-solid-svg-icons'
+export { faBars as bars } from '@fortawesome/free-solid-svg-icons'
+export { faXmark as xmark } from '@fortawesome/free-solid-svg-icons'
+export { faHome as home } from '@fortawesome/free-solid-svg-icons'
