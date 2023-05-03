35cfa1ba6b1b99722e73abc25c6a03872310998c
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-03 08:02:52 +0100

    restaurant slice

---
+++ src/features/restaurants/restaurantSlice.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+const initialState = {
+  names: [],
+  loading: false,
+  error: '',
+}
+
+export const fetchRestaurants = createAsyncThunk(
+  'restaurants/fetchRestaurants',
+  () => {
+    return axios
+      .get('http://localhost:4400/restaurants')
+      .then(response => response.data)
+  }
+)
+
+export const restaurantSlice = createSlice({
+  name: 'restaurants',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(fetchRestaurants.pending, state => {
+      state.loading = true
+    })
+    builder.addCase(fetchRestaurants.fulfilled, (state, action) => {
+      state.loading = false
+      state.names = action.payload
+      state.error = ''
+    })
+    builder.addCase(fetchRestaurants.rejected, (state, action) => {
+      state.loading = false
+      state.names = []
+      state.error = action.error.message || 'Unable to fetch restaurants'
+    })
+  },
+})
+
+// Action creators are generated for each case reducer function
+// export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+export default restaurantSlice.reducer
