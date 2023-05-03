419239007294706fdb96cffd4918a2f57c5f70c7
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-03 06:51:13 +0100

    organized imports

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from '@compo/Home'
+export { default as Footer } from '@layout/Footer'
+export { default as Header } from '@layout/Header'
+export { default as Compo } from '@compo/Compo'
+export { default as Restaurants } from './restaurants'
+export { default as OneRestaurant } from './OneRestaurant'
