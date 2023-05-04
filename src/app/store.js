7099390e7460e02197580caa90af79a4c4bfeef3
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-04 20:55:17 +0100

    added more reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../features/auth/authSlice'
-import counterReducer from '../features/counter/counterSlice'
-import pathReducer from '../features/path/pathSlice'
+import authReducer from '@features/auth/authSlice'
+import counterReducer from '@features/counter/counterSlice'
+import pathReducer from '@features/path/pathSlice'
+import restaurantReducer from '@features/restaurants/restaurantSlice'
+import userReducer from '@features/user/userSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     counter: counterReducer,
     path: pathReducer,
+    restaurants: restaurantReducer,
+    user: userReducer,
   },
 })
