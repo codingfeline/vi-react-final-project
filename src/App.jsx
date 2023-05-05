1b733b39a924332f518d257e5e7aba8d288f615d
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-05 21:47:21 +0100

    bg for route /one

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,22 @@ function App() {
     <>
       <div
         className={` flex flex-col h-screen justify-between
-        ${location.pathname === '/' && 'bg-indigo-200'}
-        ${location.pathname === '/compo' && 'bg-red-200'}
-        ${location.pathname === '/restaurants' && 'bg-lime-200'}
+        ${location.pathname === '/' && 'bg1'}
+        ${location.pathname === '/compo' && 'bg2'}
+        ${location.pathname === '/restaurants' && 'bg3'}
+        ${location.pathname.includes('/one') && 'bg4'} 
         `}
       >
         <Header />
-        <div className="flex flex-col mb-auto">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/compo" element={<Compo />} />
-            <Route path="/restaurants" element={<Restaurants />} />
-            <Route path="/restaurant/:id" element={<OneRestaurant />} />
-          </Routes>
+        <div className="flex flex-col mb-auto h-screen lg:max-w-4x place-self-center w-full md:w-11/12 lg:w-3/4  bg-red-100 ">
+          <div className="py-">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/compo" element={<Compo />} />
+              <Route path="/restaurants" element={<Restaurants />} />
+              <Route path="/restaurant/one/:id" element={<OneRestaurant />} />
+            </Routes>
+          </div>
         </div>
         <Footer />
       </div>
