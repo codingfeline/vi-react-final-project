33c4fad93c4b26e85c24b279e7356361019d1411
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-13 07:29:28 +0100

    more form el tests

--- src/components/about.test.jsx
+++ src/components/about.test.jsx
@@ -5,9 +5,32 @@ import { render, screen } from '../../test-utils'
 import About from './about'
 
 describe('group', () => {
-  it('renders learn react link', () => {
+  it('renders heading by name', () => {
     render(<About />)
-    const linkElement = screen.getByText(/increment/i)
-    expect(linkElement).toBeDefined()
+    const el = screen.getByRole('heading', {
+      name: 'about',
+    })
+    expect(el).toBeDefined()
+  })
+  it('renders heading by level #', () => {
+    render(<About />)
+    const el = screen.getByRole('heading', {
+      level: 2,
+    })
+    expect(el).toBeDefined()
+  })
+  it('renders email textbox id', () => {
+    render(<About />)
+    const el = screen.getByRole('textbox', {
+      name: 'Email address',
+    })
+    expect(el).toBeDefined()
+  })
+  it('renders checkbox by label', () => {
+    render(<About />)
+    const el = screen.getByLabelText(
+      'I agree to the terms Terms and Privacy Policy'
+    )
+    expect(el).toBeDefined()
   })
 })
