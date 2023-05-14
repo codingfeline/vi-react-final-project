fbe19624f5f8fab22d17869c64fc171f34b16c2b
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-14 16:53:07 +0100

    working checkbox toggle

--- src/components/restaurants.jsx
+++ src/components/restaurants.jsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom'
 import { Awe, envelope } from './icons'
 import supabase from '../supabaseClient'
 
-function Restaurants() {
+export default function Restaurants() {
   const [items, setItems] = useState([])
   const [error, setError] = useState(null)
+  const [isOpen, setIsOpen] = useState(true)
 
   const fetchData = async () => {
     const { data, error } = await supabase.from('restaurants').select()
@@ -18,28 +19,52 @@ function Restaurants() {
       setItems(data)
     }
   }
+
+  const toggle = async (id, status) => {
+    // alert(typeof id)
+    try {
+      const { data } = await supabase
+        .from('restaurants')
+        .update({ is_open: !status })
+        .eq('id', id)
+        .throwOnError()
+        .select()
+
+      if (data) setItems(data)
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
   useEffect(() => {
     fetchData()
-  }, [])
+  }, [items])
 
   return (
     <>
       <h1 className="bg-yellow-100 text-lg tracking-[0.8em]">Restaurants</h1>
       <Awe icon={envelope} size="2x" />
-      {items.map(item => (
-        <div className="flex justify-between items-center gap-2 w-3/5">
-          <Link
-            className="z-50 block bg-blue-200  hover:bg-indigo-300 p-1"
-            to={`/restaurant/one/${item.id}`}
-            key={item.id}
-          >
-            {item.name}
-          </Link>
-          <input type="checkbox" />
-        </div>
-      ))}
+      {/* {JSON.stringify(items)} */}
+      {items.map(item => {
+        return (
+          <div className="flex justify-between items-center gap-2 w-3/5">
+            <Link
+              className="z-50 block bg-blue-200  hover:bg-indigo-300 p-1"
+              to={`/restaurant/one/${item.id}`}
+              key={item.id}
+            >
+              {item.name}
+            </Link>
+            {/* {typeof item.id} */}
+            {item.isOpen}
+            <input
+              type="checkbox"
+              onChange={() => toggle(item.id, item.is_open)}
+              checked={item.is_open ? true : false}
+            />
+          </div>
+        )
+      })}
     </>
   )
 }
-
-export default Restaurants
